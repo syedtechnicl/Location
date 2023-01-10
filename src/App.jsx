f8f062f84dfebf8b070f54be94de11baad438c3f
Syed <83526948+syed8867@users.noreply.github.com>
2023-01-10 23:45:47 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,45 @@
-import React from 'react'
-import Nav from './Nav'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import About from './About'
-import Contact from './Contact'
+import { useReducer } from "react";
+const initialstate = 0;
+const reduce = (state, action) => {
+  console.log(state, action);
+  if (action.type === "INCREMENT") {
+    return state + 1;
+  }
+  if (action.type === "DECREMENT") {
+    return state - 1;
+  }
+  return state;
+};
+
 const App = () => {
+  const [state, dispach] = useReducer(reduce, initialstate);
+
+  const Result = () => {
+    if (state <= 1) {
+      alert("lot of limitem ---");
+    } else {
+      const names = () => dispach({ type: "DECREMENT" });
+      names();
+    }
+  };
+
   return (
-<>
-<Router>
-  <Nav/>
-  <Routes>
-     <Route path='/'/>
-    <Route path='/about' element={<About/>}/>
-    <Route path='/contact' element={<Contact/>}/>
+    <>
+      <div>
+        <p>{state}</p>
+        <div className="container">
+          <button onClick={() => dispach({ type: "INCREMENT" })}>INC</button>
+          <button onClick={Result}>DEC</button>
+        </div>
+      </div>
+    </>
+  );
+};
+export default App;
 
-
-
-  </Routes>
-</Router>
-</>
-    )
-}
-
-export default App
+git init
+git add -A
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/syedtechnicl/reduce.git
+git push -u origin main
